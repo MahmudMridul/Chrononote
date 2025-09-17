64b3d4348c5913f24ca931669180133de52ef364
Abdullah Al Mahmud <mahmudmridul.185@gmail.com>
2025-09-17 16:58:06 +0600

    Add a test signup api

---
+++ CnoteApi/CnoteApi/Controllers/AuthController.cs
@@ -0,0 +1,56 @@
+﻿using CnoteApi.Dtos;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace CnoteApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        [HttpPost("signup")]
+        public async Task<ActionResult<ApiResponse>> Signup([FromBody] SignupDto signupDto)
+        {
+            var response = new ApiResponse()
+            {
+                Data = signupDto,
+                Message = "User registration test",
+                Success = true,
+                StatusCode = HttpStatusCode.OK
+            };
+            return Ok(response);
+        }
+
+        // GET: api/<AuthController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<AuthController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<AuthController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<AuthController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<AuthController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
