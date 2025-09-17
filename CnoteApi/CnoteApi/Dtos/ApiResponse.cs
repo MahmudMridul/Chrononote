8ab26c06617b5abf848e5da0b2d444a225a7e70c
Abdullah Al Mahmud <mahmudmridul.185@gmail.com>
2025-09-17 16:53:19 +0600

    Add common api response #1 #2

---
+++ CnoteApi/CnoteApi/Dtos/ApiResponse.cs
@@ -0,0 +1,12 @@
+﻿using System.Net;
+
+namespace CnoteApi.Dtos
+{
+    public class ApiResponse
+    {
+        public object? Data { get; set; }
+        public string Message { get; set; } = string.Empty;
+        public bool Success { get; set; }
+        public HttpStatusCode StatusCode { get; set; }
+    }
+}
