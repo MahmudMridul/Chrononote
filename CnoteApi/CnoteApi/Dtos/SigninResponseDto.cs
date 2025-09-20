6d2b966dac362403983d07223a3258e4975c5b89
Abdullah Al Mahmud <mahmudmridul.185@gmail.com>
2025-09-20 18:32:30 +0600

    Add sign in api with proper response and cookie settings

---
+++ CnoteApi/CnoteApi/Dtos/SigninResponseDto.cs
@@ -0,0 +1,16 @@
+﻿using CnoteApi.Models;
+
+namespace CnoteApi.Dtos
+{
+    public class SigninResponseDto
+    {
+        public int UserId { get; set; }
+        public string Username { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        public string AccessToken { get; set; } = null!;
+        public string RefreshToken { get; set; } = null!;
+        public DateTime AccessTokenExpiresAt { get; set; }
+        public DateTime RefreshTokenExpiresAt { get; set; }
+
+    }
+}
