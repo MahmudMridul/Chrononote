9b14c9c619497f049ce6333cbb6c098347e33637
Abdullah Al Mahmud <mahmudmridul.185@gmail.com>
2025-09-25 19:30:40 +0600

    Add Project and Timesheet model #31

---
+++ CnoteApi/CnoteApi/Models/Project.cs
@@ -0,0 +1,9 @@
+﻿namespace CnoteApi.Models
+{
+    public class Project
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public ICollection<TimeCard> TimeCards { get; set; } = new List<TimeCard>();
+    }
+}
---
+++ CnoteApi/CnoteApi/Models/TimeCard.cs
@@ -0,0 +1,12 @@
+﻿namespace CnoteApi.Models
+{
+    public class TimeCard
+    {
+        public int Id { get; set; }
+        public int DurationInMins { get; set; }
+        public DateTime Date { get; set; }
+        public string DayOfWeek { get; set; } = null!;
+        public int ProjectId { get; set; }
+        public Project Project { get; set; } = null!;
+    }
+}
