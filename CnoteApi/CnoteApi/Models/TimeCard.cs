d1bacd0dfa9373939140985d4cf1a99f6a6a52ea
Abdullah Al Mahmud <mahmudmridul.185@gmail.com>
2025-09-26 19:47:46 +0600

    Create function to convert dto to timecard object #31

--- CnoteApi/CnoteApi/Models/TimeCard.cs
+++ CnoteApi/CnoteApi/Models/TimeCard.cs
@@ -1,4 +1,6 @@
-﻿namespace CnoteApi.Models
+﻿using CnoteApi.Dtos;
+
+namespace CnoteApi.Models
 {
     public class TimeCard
     {
@@ -8,5 +10,16 @@
         public string DayOfWeek { get; set; } = null!;
         public int ProjectId { get; set; }
         public Project Project { get; set; } = null!;
+
+        public static TimeCard DtoToTimeCard(TimeCardDto dto)
+        {
+            return new TimeCard
+            {
+                DurationInMins = dto.DurationInMins,
+                Date = dto.Date,
+                DayOfWeek = dto.DayOfWeek,
+                ProjectId = dto.ProjectId,
+            };
+        }
     }
 }
