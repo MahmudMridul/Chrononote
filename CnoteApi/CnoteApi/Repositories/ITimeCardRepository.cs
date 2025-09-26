89fda60a5a249fa55c5d4db873879748162910e9
Abdullah Al Mahmud <mahmudmridul.185@gmail.com>
2025-09-26 19:49:35 +0600

    Add time card repository #31

---
+++ CnoteApi/CnoteApi/Repositories/ITimeCardRepository.cs
@@ -0,0 +1,10 @@
+﻿using CnoteApi.Models;
+
+namespace CnoteApi.Repositories
+{
+    public interface ITimeCardRepository
+    {
+        public Task<IEnumerable<TimeCard>> GetCurrentWeekTimeCard();
+        public Task<TimeCard> AddTimeCard(TimeCard timeCard);
+    }
+}
