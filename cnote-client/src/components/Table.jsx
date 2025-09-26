850eac1447d2ff71966448452a13aaa165e9ecbd
Abdullah Al Mahmud <mahmudmridul.185@gmail.com>
2025-09-26 23:12:51 +0600

    Add time sheet page and time sheet entry modal #30

--- cnote-client/src/components/Table.jsx
+++ cnote-client/src/components/Table.jsx
@@ -13,7 +13,96 @@ export default function Table() {
           <th className="px-4 py-2">Sunday</th>
         </tr>
       </thead>
-      <tbody></tbody>
+      <tbody>
+        {dummyTimeCards.map((timeCard) => {
+          return (
+            <tr key={timeCard.id} className="bg-gray-800 text-white">
+              <td className="border px-4 py-2">Project {timeCard.projectId}</td>
+              <td className="border px-4 py-2">
+                {timeCard.dayofWeek === "Monday" ? timeCard.durationInMins : ""}
+              </td>
+              <td className="border px-4 py-2">
+                {timeCard.dayofWeek === "Tuesday"
+                  ? timeCard.durationInMins
+                  : ""}
+              </td>
+              <td className="border px-4 py-2">
+                {timeCard.dayofWeek === "Wednesday"
+                  ? timeCard.durationInMins
+                  : ""}
+              </td>
+              <td className="border px-4 py-2">
+                {timeCard.dayofWeek === "Thursday"
+                  ? timeCard.durationInMins
+                  : ""}
+              </td>
+              <td className="border px-4 py-2">
+                {timeCard.dayofWeek === "Friday" ? timeCard.durationInMins : ""}
+              </td>
+              <td className="border px-4 py-2">
+                {timeCard.dayofWeek === "Saturday"
+                  ? timeCard.durationInMins
+                  : ""}
+              </td>
+              <td className="border px-4 py-2">
+                {timeCard.dayofWeek === "Sunday" ? timeCard.durationInMins : ""}
+              </td>
+            </tr>
+          );
+        })}
+      </tbody>
     </table>
   );
 }
+
+const dummyTimeCards = [
+  {
+    id: 1,
+    durationInMins: 120,
+    date: "2023-10-01",
+    dayofWeek: "Monday",
+    projectId: 1,
+  },
+  {
+    id: 2,
+    durationInMins: 90,
+    date: "2023-10-02",
+    dayofWeek: "Tuesday",
+    projectId: 1,
+  },
+  {
+    id: 3,
+    durationInMins: 60,
+    date: "2023-10-03",
+    dayofWeek: "Wednesday",
+    projectId: 2,
+  },
+  {
+    id: 4,
+    durationInMins: 30,
+    date: "2023-10-04",
+    dayofWeek: "Thursday",
+    projectId: 2,
+  },
+  {
+    id: 5,
+    durationInMins: 45,
+    date: "2023-10-05",
+    dayofWeek: "Friday",
+    projectId: 3,
+  },
+  {
+    id: 6,
+    durationInMins: 150,
+    date: "2023-10-06",
+    dayofWeek: "Saturday",
+    projectId: 3,
+  },
+  {
+    id: 7,
+    durationInMins: 200,
+    date: "2023-10-07",
+    dayofWeek: "Sunday",
+    projectId: 3,
+  },
+];
